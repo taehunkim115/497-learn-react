661b32f433c5bd0ed46808382eb0a4108c5fe8ae
taehunkim115 <wlehwk115@gmail.com>
2019-10-04 12:41:12 -0500

    grid, images, descriptions, prices added; need to align cards and add size

--- new-shopping-cart/src/App.js
+++ new-shopping-cart/src/App.js
@@ -1,8 +1,65 @@
 import React, { useEffect, useState } from 'react';
+import 'rbx/index.css';
+import { Button, Container, Title, Card, Image, Content, Column } from 'rbx';
+
+const Sizes = ['XS','S','M','L','XL']
+
+const Banner = () => (
+  <React.Fragment>
+    <Title>{ 'Shopping Cart' }</Title>
+  </React.Fragment>
+);
+
+const Product = ({ products }) => (
+  <React.Fragment>
+    <Column size = 'one-quarter'>
+      <Card>
+        <Card.Image>
+          <Image.Container size = '3by4'>
+            <Image src = {`data/products/${products.sku}_1.jpg`} />
+          </Image.Container>
+        </Card.Image>
+        <Card.Content>
+          <Content>
+            <Title>{ products.title }</Title>
+            <p>
+              { products.description} 
+            </p>
+            <p>
+            { products.style }
+            </p>
+          </Content>
+        </Card.Content>
+        <Card.Footer>
+          <Card.Footer.Item>
+            { products.currencyFormat }{ products.price }
+          </Card.Footer.Item>
+          <Card.Footer.Item>
+            Add to cart
+          </Card.Footer.Item>
+        </Card.Footer>
+      </Card>
+    </Column>
+  </React.Fragment>
+);
+
+const ProductList = ({ products, stateProduct }) => {
+  const ProductDisplay = products
+
+return (
+    <React.Fragment>
+      <Column.Group multiline>
+        { ProductDisplay.map(products => <Product key = { products.sku } products = { products }/>)}
+      </Column.Group>
+    </React.Fragment>
+  );
+};
 
 const App = () => {
   const [data, setData] = useState({});
   const products = Object.values(data);
+  const [ProductDisplay, setProductDisplay] = useState([]);
+
   useEffect(() => {
     const fetchProducts = async () => {
       const response = await fetch('./data/products.json');
@@ -14,7 +71,10 @@ const App = () => {
 
   return (
     <ul>
-      {products.map(product => <li key={product.sku}>{product.title}</li>)}
+      <Container>
+        <Banner/>
+        <ProductList products={ products } stateProduct={ { ProductDisplay, setProductDisplay} }/>
+      </Container>
     </ul>
   );
 };
