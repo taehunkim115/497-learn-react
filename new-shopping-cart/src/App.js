eeb79e18be7d9eaee204c6052f1260945c9f9cfe
taehunkim115 <wlehwk115@gmail.com>
2019-10-06 10:32:40 -0500

    shopping cart visible

--- new-shopping-cart/src/App.js
+++ new-shopping-cart/src/App.js
@@ -1,35 +1,214 @@
 import React, { useEffect, useState } from 'react';
 import 'rbx/index.css';
-import { Button, Container, Title, Card, Image, Content, Column, Notification, Navbar } from 'rbx';
+import { Button, Container, Title, Card, Image, Content, Column, Notification } from 'rbx';
+import ShoppingCart from '@material-ui/icons/ShoppingCart';
+import DeleteIcon from '@material-ui/icons/Delete';
+import Button2 from '@material-ui/core/Button';
+import { makeStyles, AppBar, Toolbar, Typography, IconButton, MenuItem, Menu, GridList, GridListTile, GridListTileBar } from '@material-ui/core';
 
 const sizes = ['S','M','L','XL'];
 
-const Banner = () => (
-  <React.Fragment>
-    <Navbar>
-      <Navbar.Brand>
-        <Navbar.Item>
-          <strong>Shopping</strong>
-        </Navbar.Item>
-      </Navbar.Brand>
-      <Navbar.Menu>
-        <Navbar.Segment align = 'end'>
-          <Navbar.Item>
-            <Button.Group>
-              <Button color = 'primary'>
-                <strong>Sign up</strong>
-              </Button>
-              <Button color = 'light'>Log in</Button>
-              <Button color = 'danger'> 
-              <strong>Cart</strong>
-              </Button>
-            </Button.Group>
-          </Navbar.Item>
-        </Navbar.Segment>
-      </Navbar.Menu>
-    </Navbar>
-  </React.Fragment>
-);
+const staticcart = {
+  "12064273040195392": {
+    "sku": 12064273040195392,
+    "title": "Cat Tee Black T-Shirt",
+    "description": "4 MSL",
+    "style": "Black with custom print",
+    "price": 10.9,
+    "currencyId": "USD",
+    "currencyFormat": "$",
+    "isFreeShipping": true
+  },
+  "51498472915966370": {
+    "sku": 51498472915966370,
+    "title": "Dark Thug Blue-Navy T-Shirt",
+    "description": "",
+    "style": "Front print and paisley print",
+    "price": 29.45,
+    "currencyId": "USD",
+    "currencyFormat": "$",
+    "isFreeShipping": true
+  },
+  "10686354557628304": {
+    "sku": 10686354557628304,
+    "title": "Sphynx Tie Dye Wine T-Shirt",
+    "description": "GPX Poly 1",
+    "style": "Front tie dye print",
+    "price": 9,
+    "currencyId": "USD",
+    "currencyFormat": "$",
+    "isFreeShipping": true
+  },
+  "11033926921508488": {
+    "sku": 11033926921508488,
+    "title": "Skuul",
+    "description": "Training 2014",
+    "style": "Black T-Shirt with front print",
+    "price": 14,
+    "currencyId": "USD",
+    "currencyFormat": "$",
+    "isFreeShipping": true
+  },
+  "39876704341265610": {
+    "sku": 39876704341265610,
+    "title": "Wine Skul T-Shirt",
+    "description": "",
+    "style": "Wine",
+    "price": 13.25,
+    "currencyId": "USD",
+    "currencyFormat": "$",
+    "isFreeShipping": true
+  },
+};
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1
+  }
+}));
+
+const useStylescart = makeStyles(theme => ({
+  root: {
+    width: 200,
+    marginRight:8
+  },
+  gridlisttilebar: {
+    display:'flex',
+    height: 100
+  }
+}));
+
+const useStylesgrid = makeStyles(theme => ({
+  root: {
+    display:'flex',
+    flexDirection:'column',
+  },
+  container: {
+    alignItems:'flex-start',
+    justifyContent:'flex-start',
+    paddingTop:10,
+    height: 300,
+  },
+  gridList: {
+      height:280,
+      flexWrap: 'nowrap',
+      flexGrow:1,
+      transform: 'translateZ(0)',
+    }
+}));
+
+const Banner = () => {
+  const classes = useStyles();
+  const [cart, setCart] = React.useState(true);
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+
+  const handleChange = event => {
+    setCart(event.target.checked);
+  };
+  
+  const handleMenu = event => {
+    setAnchorEl(event.currentTarget);
+  };
+  
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return(
+    <React.Fragment className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" className={classes.title}>
+            Shopping
+          </Typography>
+          <Button2 color="inherit">Sign up</Button2>
+          <Button2 color="inherit">Log in</Button2>
+          {cart && (
+            <React.Fragment>
+              <IconButton
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                onClick={handleMenu}
+                color="inherit"
+              >
+                <ShoppingCart />
+              </IconButton>
+              <Menu
+                id="menu-appbar"
+                anchorEl={anchorEl}
+                anchorOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                open={open}
+                onClose={handleClose}
+              >
+                <MenuItem>
+                  <strong>Your Cart</strong>
+                </MenuItem>
+                <MenuItem>
+                <CartList products={ staticcart } />
+                </MenuItem>
+                <MenuItem>
+                  Total Price: XX
+                </MenuItem>
+                <MenuItem onClick={handleClose}>Close Cart</MenuItem>
+              </Menu>
+            </React.Fragment>
+          )}
+        </Toolbar>
+      </AppBar>
+    </React.Fragment>
+  );
+};
+
+const ProductCard = ({ products }) => {
+  const classes = useStylescart();
+  const size = "M"
+  const quantity = 2;
+
+  const caption = (
+    <React.Fragment>
+      <Typography variant='body2'>
+        Size: {size}
+      </Typography>
+      <Typography variant='body2'>
+        Quantity: {quantity}
+      </Typography>
+      <Typography variant='body2'>
+        Price: {products.currencyFormat}{products.price}
+      </Typography>
+    </React.Fragment>
+  );
+
+  return(
+    <GridListTile key={products.sku} className={classes.root}>       
+      <img src={`./data/products/${products.sku}_2.jpg`} alt={products.title}/>
+    <GridListTileBar className={classes.gridlisttilebar}
+      title={products.title}
+      subtitle={caption}
+      actionIcon={
+        <IconButton color='inherit'>
+          <DeleteIcon />
+        </IconButton>
+        }
+    />
+    </GridListTile>
+  )
+};
 
 const Product = ({ products }) => (
   <React.Fragment>
@@ -37,7 +216,7 @@ const Product = ({ products }) => (
       <Card>
         <Card.Image>
           <Image.Container size = '3by4'>
-            <Image src = {`data/products/${products.sku}_1.jpg`} />
+            <Image src = {`./data/products/${products.sku}_1.jpg`} />
           </Image.Container>
         </Card.Image>
         <Card.Content>
@@ -61,7 +240,7 @@ const Product = ({ products }) => (
                   { products.currencyFormat }{ products.price }
                 </Card.Footer.Item>
                 <Card.Footer.Item>
-                  Add to cart
+                  <Button color='primary'>Add to cart</Button>
                 </Card.Footer.Item>
               </Column.Group>
           </Column>
@@ -71,7 +250,18 @@ const Product = ({ products }) => (
   </React.Fragment>
 );
 
+const CartList = ({ products }) => {
+  const classes = useStylesgrid();
+  const data = Object.values(products);
 
+  return (
+    <React.Fragment className={classes.root}>
+        <GridList className={classes.gridList}>
+          { data.map(products => <ProductCard key = { products.sku } products = { products }/>)}
+        </GridList>
+    </React.Fragment>
+  );
+};
 
 const ProductList = ({ products }) => {
 
@@ -85,7 +275,7 @@ return (
 };
 
 const SizeButtons = () => (
-  <Button.Group align = 'centered'>
+  <Button.Group align = "centered">
       { Object.values(sizes)
         .map(value =>
           <Button rounded key = { value } >
